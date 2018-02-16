d25823bbd531cdca53f7530f436349bc07b97cab
Sami Konzo <samikonzo@gmail.com>
2018-02-16 15:11:46 +0300

    :commit

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -24,56 +24,14 @@ class App extends React.Component{
 
 		this.handleNoteAdd = this.handleNoteAdd.bind(this)
 		this.handleNoteDelete = this.handleNoteDelete.bind(this)
-		this.getNotes = this.getNotes.bind(this)
-
-		this.getNotes()
 	}
 
 	handleNoteAdd(note){
-		l(note)
-
-		var that = this
-
-		//add server request
-		var xhr = new XMLHttpRequest()
-		xhr.open('POST', '/api/addNote')
-		xhr.setRequestHeader("Content-type", "application/json");
-		xhr.send(JSON.stringify(note))
-
-		xhr.onload = function(){
-			var item = JSON.parse(this.response)
-			var notes = that.state.notes
-			notes.push(item)
-
-			that.setState({
-				notes: notes
-			})
-		}
 	}
 
 	handleNoteDelete(note){
-		//add server request
-		l(note)
-		var xhr = new XMLHttpRequest()
-		xhr.open('DELETE', '/api/deleteNote' + note.id)
-		xhr.send()
-		note.remove()
-		//note.delete()
 	}
 
-	getNotes(){
-		var xhr = new XMLHttpRequest
-		var that = this
-		xhr.open('GET', '/api/notes')
-		xhr.send()
-
-		xhr.onload = function(){
-			var notes = JSON.parse(this.response)
-			that.setState({
-				notes: notes
-			})
-		}
-	}
 
 	render(){
 		return (
--- client/stores/NotesStore.js
+++ client/stores/NotesStore.js
@@ -2,3 +2,65 @@ import { EventEmitter } from 'events'
 import Dispatcher from '../dispatcher/FluxDispatcher.js'
 import Constants from '../constants/FluxConstants.js'
 
+const CHANGE_EVENT = 'change'
+
+let _notes = []
+let _loadingError = null
+let _isLoading = true
+
+function formatNote(note) {
+	return{
+		id: note._id,
+		title: note.title,
+		text: note.text,
+		color: note.color || '#ffffff',
+		createdDate: note.createdDate
+	}
+}
+
+const TasksStore = Object.assign({}, EventEmitter.prototype, {
+	isLoading(){
+		return _isLoading
+	},
+
+	getNotes(){
+		return _notes
+	},
+
+	emitChange(){
+		this.emit(CHANGE_EVENT)
+	},
+
+	addChangeListener(f){
+		this.on(CHANGE_EVENT, f)
+	}
+
+	removeChangeListener(f){
+		this.removeListener(CHANGE_EVENT,f)
+	}
+})
+
+Dispatcher.register(function(action){
+	switch(action.type){
+		case Constants.LOAD_NOTES_REQUEST : {
+			_isLoading = true
+
+			TasksStore.emitChange()
+			break
+		}
+
+		case Constants.LOAD_NOTES_SUCCESS : {
+			_isLoading = false
+			_notes = action.notes.map( formatNote )
+			_loadingError = null
+			
+			TasksStore.emitChange()
+			break
+		}
+
+		case Constants.LOAD_NOTES_FAIL : {
+			
+			break
+		}
+	}
+})
